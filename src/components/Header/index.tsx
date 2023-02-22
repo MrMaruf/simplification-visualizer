14d43f568376f359cce75f29fa77fb668abbca9e
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-02-22 21:39:54 +0100

    Temporary navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,62 @@
+"use client";
+
 import React from "react";
 import styles from "./index.module.css";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
+type Page = {
+  name:string;
+  url:string;
+}
+const pages:Page[] = [{name:"Sorting Algorithms", url:"/algorithms/sorting/"}];
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
     <header className={styles.header}>
-      <p>Visual Simplifier</p>
-      <div>
-        <a
-          href="https://www.linkedin.com/in/maruf-abdirimov-421b56184/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          By Maruf Abdirimov
-        </a>
-      </div>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+                textAlign: "left",
+              }}
+            >
+              Visual Simplifier
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Typography
+                  key={page.name}
+                  component="a"
+                  href={page.url}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page.name}
+                </Typography>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </header>
   );
 };
