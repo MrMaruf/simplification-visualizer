926c89a6296154646bf28f975b64ceafc27e344f
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-02-22 21:40:17 +0100

    Base for first sorting algorithm

---
+++ src/app/algorithms/sorting/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SortingAlgorithmsHome = (props: Props) => {
+  return (
+    <div>SortingAlgorithmsHome</div>
+  )
+}
+
+export default SortingAlgorithmsHome
---
+++ src/app/algorithms/sorting/insertion/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './index.module.css'
+
+type Props = {}
+
+const InsertionSortingAlgorithm = (props: Props) => {
+  return (
+    <div>InsertionSortingAlgorithm</div>
+  )
+}
+
+export default InsertionSortingAlgorithm
