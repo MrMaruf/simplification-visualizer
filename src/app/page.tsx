860e39aa30120184241d73d614415b66772cac7d
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-02-23 18:49:33 +0100

    Cleaned up the imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
-import { Header } from "@/components";
+import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
