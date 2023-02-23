b3c2534756aeae29f51c70d614ee11f8cf2264dd
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-02-23 18:47:43 +0100

    Started working on breadcrumbs for better navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,52 +9,86 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
+import { usePathname } from "next/navigation";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
 
 type Page = {
-  name:string;
-  url:string;
-}
-const pages:Page[] = [{name:"Sorting Algorithms", url:"/algorithms/sorting/"}];
+  name: string;
+  url: string;
+};
+const pages: Page[] = [
+  { name: "Sorting Algorithms", url: "/algorithms/sorting/" },
+];
 
 type Props = {};
 
 const Header = (props: Props) => {
+  const path = usePathname();
+  console.log("'", path, "'");
+  const navigation = path?.split("/");
+  console.log(navigation);
+
   return (
     <header className={styles.header}>
       <AppBar position="static">
         <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-                textAlign: "left",
-              }}
-            >
-              Visual Simplifier
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              {pages.map((page) => (
-                <Typography
-                  key={page.name}
-                  component="a"
-                  href={page.url}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  {page.name}
-                </Typography>
-              ))}
-            </Box>
-          </Toolbar>
+          <Stack>
+            <div className={styles.breadcrumbs}>
+              <Breadcrumbs aria-label="breadcrumb">
+                <Link underline="hover" color="inherit" href="/">
+                  Visual Simplifier
+                </Link>
+                {navigation?.map((path, index) => {
+                  const trimmedPath = path.trim();
+                  if (trimmedPath.length > 0)
+                    return (
+                      <Link
+                        underline="hover"
+                        color="inherit"
+                        href={path}
+                        key={path}
+                      >
+                        {trimmedPath}
+                      </Link>
+                    );
+                })}
+              </Breadcrumbs>
+            </div>
+            <Toolbar disableGutters>
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                  textAlign: "left",
+                }}
+              >
+                Visual Simplifier
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                {pages.map((page) => (
+                  <Typography
+                    key={page.name}
+                    component="a"
+                    href={page.url}
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    {page.name}
+                  </Typography>
+                ))}
+              </Box>
+            </Toolbar>
+          </Stack>
         </Container>
       </AppBar>
     </header>
