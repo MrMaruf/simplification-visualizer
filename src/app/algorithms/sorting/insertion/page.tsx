05a7dbe5a3b2e2783809cdfc40ad4768e30e22d7
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-02-25 21:41:43 +0100

    Working on styling and initial sorting

--- src/app/algorithms/sorting/insertion/page.tsx
+++ src/app/algorithms/sorting/insertion/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
+import List from "@/components/List";
 import Box from "@mui/material/Box";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Unstable_Grid2";
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import styles from "./index.module.css";
 
 type Props = {};
@@ -22,50 +21,72 @@ function shuffleArray(array: any[]) {
 }
 
 const InsertionSortingAlgorithm = (props: Props) => {
-  const shuffledItems = useMemo(() => shuffleArray([...items]), [items]);
+  const [shuffledItems, setShuffledItems] = useState([...items]);
+  const [sortedItems, setSortedArray] = useState([...items]);
+
+  const onShuffle = () => {
+    setShuffledItems((value) => {
+      const newShuffle = shuffleArray([...value])
+      setSortedArray([...newShuffle])
+      return newShuffle
+    });
+    
+  };
+  const onSort = () => {
+    const toSort = [...shuffledItems];
+    const length = shuffledItems.length;
+    for (let index = 1; index < length - 1; index++) {
+      let index2 = index;
+      do {
+        let elementLeft = toSort[index2 - 1];
+        let elementRight = toSort[index2];
+        if (elementLeft < elementRight) break;
+        toSort[index2] = elementLeft;
+        toSort[index2 - 1] = elementRight;
+        index2--;
+      } while (index2 > 0);
+    }
+    setSortedArray(toSort);
+  };
   return (
     <Box margin="15px 10px">
       <h1>Insertion Algorithm</h1>
-      <Grid container spacing={2} marginTop="15px">
-        <Grid
-          xs={2}
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <h2>Original Array</h2>
-          <List className={styles.list}>
-            {items.map((item) => {
-              return (
-                <ListItem key={item} disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary={item} />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-          </List>
+      <Grid container spacing={2} marginTop="15px" className={styles.stack}>
+        <Grid container xs={5}>
+          <Grid xs={5}>
+            <Button variant="contained" onClick={onShuffle}>
+              Shuffle Array
+            </Button>
+          </Grid>
+          <Grid xs={5}>
+            <Button variant="contained" onClick={onSort}>
+              Sort Array
+            </Button>
+          </Grid>
         </Grid>
-        <Grid
-          xs={2}
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <h2>Shuffled Array</h2>
-          <List className={styles.list}>
-            {shuffledItems.map((item) => {
-              return (
-                <ListItem key={item} disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary={item} />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-          </List>
+        <Grid container xs={8}>
+          <Grid display="flex" xs={4}>
+            <h2>Original Array</h2>
+          </Grid>
+          <Grid xs={8}>
+            <List className={styles.list} items={items} />
+          </Grid>
+        </Grid>
+        <Grid container xs={8}>
+          <Grid display="flex" xs={4}>
+            <h2>Shuffled Array</h2>
+          </Grid>
+          <Grid xs={8}>
+            <List className={styles.list} items={shuffledItems} />
+          </Grid>
+        </Grid>
+        <Grid container xs={8}>
+          <Grid display="flex" xs={4}>
+            <h2>Sorted Array</h2>
+          </Grid>
+          <Grid xs={8}>
+            <List className={styles.list} items={sortedItems} />
+          </Grid>
         </Grid>
       </Grid>
     </Box>
