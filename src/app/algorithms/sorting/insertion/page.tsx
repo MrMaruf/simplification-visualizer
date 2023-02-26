d27c1399393166f2e41054f4ae4cf20f76295177
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-02-26 22:46:48 +0100

    Working on more granular sorting & suffling process

--- src/app/algorithms/sorting/insertion/page.tsx
+++ src/app/algorithms/sorting/insertion/page.tsx
@@ -6,12 +6,12 @@ import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Unstable_Grid2";
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import styles from "./index.module.css";
 
 type Props = {};
 
-const items = Array.from(new Array(10), (x, i) => i + 1);
+const items = Array.from(new Array(10), (x, i) => i+1);
 function shuffleArray(array: any[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -19,49 +19,93 @@ function shuffleArray(array: any[]) {
   }
   return array;
 }
-
-const sortArray = (arrayToSort: number[]) => {
-  const length = arrayToSort.length;
+const sortArray = (toSort: number[]) => {
+  const length = toSort.length;
   for (let index = 1; index < length; index++) {
     let index2 = index;
     do {
-      let elementLeft = arrayToSort[index2 - 1];
-      let elementRight = arrayToSort[index2];
+      let elementLeft = toSort[index2 - 1];
+      let elementRight = toSort[index2];
       if (elementLeft < elementRight) break;
-      arrayToSort[index2] = elementLeft;
-      arrayToSort[index2 - 1] = elementRight;
+      toSort[index2] = elementLeft;
+      toSort[index2 - 1] = elementRight;
       index2--;
     } while (index2 > 0);
   }
-  return arrayToSort;
+  return toSort;
 };
-
+//TODO: Look into animation on end stagger
+//TODO: Look into step by step sorting and shuffling.
+//TODO: Look into not only animating sorting process but only every step that goes before and after sorting.
 const InsertionSortingAlgorithm = (props: Props) => {
   const [shuffledItems, setShuffledItems] = useState<number[]>([]);
   const [sortedItems, setSortedArray] = useState<number[]>([]);
 
+  const onReset = () => {
+    setShuffledItems([]);
+    setSortedArray([]);
+  };
   const onShuffle = () => {
     const newShuffle = shuffleArray([...items]);
     setShuffledItems(newShuffle);
   };
   const onSort = () => {
-    const toSort = [...shuffledItems];
-    const sorted = sortArray(toSort);
-    setSortedArray(sorted);
+    setSortedArray([...shuffledItems]);
+    // const toSort = [...shuffledItems];
+    // const length = toSort.length;
+    // let index = 1,
+    //   index2: number;
+    // let interval = setInterval(() => {
+    //   console.log("In interval")
+    //   if (index == length) clearInterval(interval);
+    //   if (index2 <= 0) index++;
+    //   index2 = index;
+    //   const elementLeft = toSort[index2 - 1];
+    //   const elementRight = toSort[index2];
+    //   if (elementLeft < elementRight) {
+    //     index2 = 0;
+    //     return;
+    //   }
+    //   toSort[index2] = elementLeft;
+    //   toSort[index2 - 1] = elementRight;
+    //   setSortedArray([...toSort]);
+    //   index2--;
+    // }, 3000);
+    // for (let index = 1; index < length; index++) {
+    //   let index2 = index;
+    //   do {
+    //     let elementLeft = toSort[index2 - 1];
+    //     let elementRight = toSort[index2];
+    //     if (elementLeft < elementRight) break;
+    //     toSort[index2] = elementLeft;
+    //     toSort[index2 - 1] = elementRight;
+    //     index2--;
+    //   } while (index2 > 0);
+    // }
+    setTimeout(() => {
+      const toSort = [...shuffledItems];
+      const sorted = sortArray(toSort);
+      setSortedArray(sorted);
+    }, 500);
   };
   return (
     <Box margin="15px 10px">
       <h1>Insertion Algorithm</h1>
       <Grid container spacing={2} marginTop="15px" className={styles.stack}>
-        <Grid container xs={5}>
-          <Grid xs={5}>
-            <Button variant="contained" onClick={onShuffle}>
+        <Grid container xs={7}>
+          <Grid xs={4}>
+            <Button variant="contained" color="info" onClick={onShuffle}>
               Shuffle Array
             </Button>
           </Grid>
-          <Grid xs={5}>
-            <Button variant="contained" onClick={onSort}>
+          <Grid xs={4}>
+            <Button variant="contained" color="primary" onClick={onSort}>
               Sort Array
+            </Button>
+          </Grid>
+          <Grid xs={4}>
+            <Button variant="contained" color="warning" onClick={onReset}>
+              Reset Array
             </Button>
           </Grid>
         </Grid>
