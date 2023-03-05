b80c21ca6817d419991441ff9d934a1bd068a73b
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-03-05 13:09:20 +0100

    Extracted repeated list container

---
+++ src/components/ItemsArrayContainer/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Grid from "@mui/material/Unstable_Grid2";
+
+import List from "@/components/List";
+import { Items } from "@/types/store/SortingTypes";
+
+import styles from "./index.module.css";
+
+type Props = {
+  array: Items;
+  name: string;
+};
+
+const ItemsArrayContainer = (props: Props) => {
+  const { array, name } = props;
+  return (
+    <Grid container xs={8}>
+      <Grid display="flex" xs={4}>
+        <h2>{name}</h2>
+      </Grid>
+      <Grid xs={8}>
+        <List className={styles.list} items={array} />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ItemsArrayContainer;
