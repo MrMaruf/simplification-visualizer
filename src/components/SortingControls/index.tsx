0ead9cc70195623e14c331a80ff5e86106e38f30
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-03-05 13:09:35 +0100

    Extracted controls for sorting

--- src/components/SortingControls/index.tsx
+++ src/components/SortingControls/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import Grid from "@mui/material/Unstable_Grid2";
 import SortTypeSelector from "@/components/SortTypeSelector";
@@ -8,16 +8,73 @@ import Pagination from "@mui/material/Pagination";
 import Typography from "@mui/material/Typography";
 
 import styles from "./index.module.css";
+import { useSortingState } from "@/store/SortingContext";
+import { Items } from "@/types/store/SortingTypes";
 
-type Props = {};
+type Props = {
+  originalItems: Items;
+  stagedSortArray: (items: Items) => Items[];
+  sortArray: (items: Items) => Items;
+};
 
 const SortingControls = (props: Props) => {
+  const sortingCtx = useSortingState();
+  const { sortingType, items, stagingSpeed, sortingStages, currentStage } =
+    sortingCtx.state;
+  const { stagedSortArray, sortArray } = props;
+  const [isSorting, setIsSorting] = useState(false);
+  const sortingIntervalRef = useRef<NodeJS.Timer | undefined>(undefined);
+  const onReset = () => {
+    sortingCtx.dispatch({ type: "generate array", length: 10 });
+  };
+  const onShuffle = () => {
+    clearSortingInterval();
+    sortingCtx.dispatch({ type: "shuffle array" });
+  };
+  const clearSortingInterval = () => {
+    if (!sortingIntervalRef.current) return;
+    clearInterval(sortingIntervalRef.current);
+    setIsSorting(false);
+  };
+  const onSort = () => {
+    if (isSorting) return clearSortingInterval();
+    const toSort = [...items];
+    if (sortingType === "realtime")
+      return sortingCtx.dispatch({
+        type: "set items",
+        items: sortArray(toSort),
+      });
+
+    const stages = stagedSortArray(toSort);
+    sortingCtx.dispatch({ type: "set stages", stages });
+    if (sortingType === "staged automatic") startSortingInterval(stagingSpeed);
+  };
+  const startSortingInterval = (stagingSpeed: number) => {
+    setIsSorting(true);
+    sortingIntervalRef.current = setInterval(() => {
+      sortingCtx.dispatch({ type: "move stage", onEnd: clearSortingInterval });
+    }, stagingSpeed);
+  };
+  useEffect(() => {
+    return () => {
+      if (sortingIntervalRef.current) clearInterval(sortingIntervalRef.current);
+    };
+  }, []);
+
+  const onStageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    sortingCtx.dispatch({ type: "move stage", newStage: value });
+  };
   return (
     <Grid container xs={11} className={styles.controllers}>
       <Grid xs={3}>
         <SortTypeSelector
           value={sortingType}
-          onChange={(value) => setSortingType(value)}
+          onChange={(value) =>
+            sortingCtx.dispatch({
+              type: "change sorting type",
+              newType: value,
+            })
+          }
         />
       </Grid>
       <Grid xs={3}>
@@ -28,7 +85,7 @@ const SortingControls = (props: Props) => {
       <Grid xs={3}>
         <Button
           disabled={items.every(
-            (value, index) => originalItems[index] === value
+            (value, index) => props.originalItems[index] === value
           )}
           variant="contained"
           color={isSorting ? "error" : "primary"}
@@ -47,7 +104,11 @@ const SortingControls = (props: Props) => {
           <TimeSlider
             value={stagingSpeed}
             onChange={(event, value) => {
-              if (typeof value === "number") setStagingSpeed(value);
+              if (typeof value === "number")
+                sortingCtx.dispatch({
+                  type: "change sorting speed",
+                  newSpeed: value,
+                });
             }}
             disabled={isSorting}
           />
