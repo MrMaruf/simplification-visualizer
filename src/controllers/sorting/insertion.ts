0443a5f69f7f614810a85c305b934bb0800def33
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-03-08 23:48:02 +0100

    Started working on more verbose staging

--- src/controllers/sorting/insertion.ts
+++ src/controllers/sorting/insertion.ts
@@ -26,27 +26,38 @@ export const stagedSortArray = (
   for (let index = 1; index < length; index++) {
     let index2 = index;
     do {
-      let elementLeft = toSort[index2 - 1];
-      let elementRight = toSort[index2];
-      if (elementLeft.name < elementRight.name) break;
-      const stage: Stage = {
+      let elementLeft: Item = toSort[index2 - 1];
+      let elementRight: Item = toSort[index2];
+      elementLeft.className = comparableItemClass;
+      elementRight.className = comparingItemClass;
+      const preCompareStage: Stage = {
         name: "Swapping",
-        description: `Swapping ${elementRight} with ${elementLeft}`,
+        description: `Comparing current item(${elementRight.name}) with previous item(${elementLeft.name})`,
+        followUp: `If current item is less than previous one, swap, otherwise proceed.`,
+        items: [...toSort],
+      };
+      stages.push(preCompareStage);
+      if (elementLeft.name < elementRight.name) break;
+      toSort[index2] = elementLeft;
+      toSort[index2 - 1] = elementRight;
+      const swappingStage: Stage = {
+        name: "Swapping",
+        description: `Swapping ${elementRight.name} with ${elementLeft.name}`,
         followUp: `Proceed to the next number`,
         items: [...toSort],
       };
-      stages.push(stage);
-      toSort[index2] = elementLeft;
-      toSort[index2 - 1] = elementRight;
+      elementLeft.className = undefined;
+      elementRight.className = undefined;
+      stages.push(swappingStage);
       index2--;
     } while (index2 > 0);
   }
-  const stage: Stage = {
+  const lastStage: Stage = {
     name: "Swapping",
     description: `Swapping last element`,
     followUp: `Proceed to the next number`,
     items: [...toSort],
   };
-  stages.push(stage); // add last stage
+  stages.push(lastStage); // add last stage
   return stages;
 };
