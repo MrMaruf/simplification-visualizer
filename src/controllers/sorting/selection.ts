adcccf57bfcabca3b82fd5540d0c724afc4f9e75
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-03-19 19:01:20 +0100

    Fixing forever loop, fixing the rest of the method

--- src/controllers/sorting/selection.ts
+++ src/controllers/sorting/selection.ts
@@ -5,12 +5,16 @@ export const sortArray = (toSort: Item[]) => {
   for (let index = 0; index < length - 1; index++) {
     let index2 = index + 1;
     let minimum = toSort[index];
+    let currentMinimumIndex = index;
     do {
-      let newElement = toSort[index2];
+      let newElement = toSort[index2++];
       if (minimum.name < newElement.name) continue;
+      currentMinimumIndex = index2 - 1;
       minimum = newElement;
-      index2++;
     } while (index2 < length);
+    let previousMinimum = toSort[index];
+    toSort[index] = minimum;
+    toSort[currentMinimumIndex] = previousMinimum;
   }
   return toSort;
 };
@@ -26,10 +30,12 @@ export const stagedSortArray = (
   for (let index = 0; index < length - 1; index++) {
     let index2 = index + 1;
     let minimum = toSort[index];
+    let currentMinimumIndex = index;
     let swappingStage: Stage | undefined;
     const originalSortCopy = [...toSort];
+    let foundNewMinimum = false;
     do {
-      let newElement = toSort[index2];
+      let newElement = toSort[index2++];
       const toSortCopy: Item[] = [...originalSortCopy];
       toSortCopy[index2 - 1] = {
         ...newElement,
@@ -44,16 +50,21 @@ export const stagedSortArray = (
       };
       stages.push(preCompareStage);
       if (minimum.name < newElement.name) continue;
+      foundNewMinimum = true;
+      currentMinimumIndex = index2 - 1;
       minimum = newElement;
-      index2++;
-      swappingStage = {
-        name: `Moving ${minimum.name} to the #${index} position`,
-        description: `New minimum found! Moving to the #${index} position.`,
-        followUp: `Proceed to the next number`,
-        items: [...toSort],
-      };
     } while (index2 < length);
-    if (swappingStage) stages.push(swappingStage);
+    if (!foundNewMinimum) continue;
+    let previousMinimum = toSort[index];
+    toSort[index] = minimum;
+    toSort[currentMinimumIndex] = previousMinimum;
+    swappingStage = {
+      name: `Moving ${minimum.name} to the #${index} position`,
+      description: `New minimum found! Moving to the #${index} position.`,
+      followUp: `Proceed to the next number`,
+      items: [...toSort],
+    };
+    stages.push(swappingStage);
   } // add last stage
   const lastStage: Stage = {
     name: `Finished`,
