2bf89d3376e0a5c7b79dbadd282cd37a41194d3d
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-03-29 12:13:41 +0200

    A bit of work, still thinking on stages implementation

--- src/controllers/sorting/heap/stagedSort.ts
+++ src/controllers/sorting/heap/stagedSort.ts
@@ -1,6 +1,35 @@
 import { Item, Stage } from "@/types/store/SortingTypes";
 
 //TODO: Implement staging sort
+const swap = (toSort: Item[], index1: number, index2: number) => {
+  const item1 = toSort[index1];
+  const item2 = toSort[index2];
+  toSort[index2] = item1;
+  toSort[index1] = item2;
+};
+
+const heapify = (
+  toSort: Item[],
+  length: number,
+  currentIndex: number,
+  stages: Stage[] = []
+) => {
+  let largest = currentIndex;
+  const left = 2 * currentIndex + 1;
+  const right = 2 * currentIndex + 2;
+  const leftItem = toSort[left];
+  const rightItem = toSort[right];
+  if (left < length && toSort[largest].name < leftItem.name) largest = left;
+
+  if (right < length && toSort[largest].name < rightItem.name) largest = right;
+
+  // Swap and continue heapifying if root is not largest
+  if (largest != currentIndex) {
+    swap(toSort, currentIndex, largest);
+    heapify(toSort, length, largest, stages);
+  }
+};
+
 const stagedSortArray = (
   comparingItemClass: string,
   comparableItemClass: string,
@@ -9,7 +38,16 @@ const stagedSortArray = (
 ) => {
   const stages: Stage[] = [];
   const length = toSort.length;
-
+  for (let index = length / 2; index > -1; index--) {
+    console.log("Creating max heap");
+    heapify(toSort, length, index);
+  }
+  for (let index = length - 1; index > -1; index--) {
+    swap(toSort, index, 0);
+    heapify(toSort, index, 0);
+  }
+  console.log("Finished Sorting", toSort);
+  return toSort;
   for (let index = 0; index < length - 1; index++) {
     let index2 = index + 1;
     let minimum = toSort[index];
