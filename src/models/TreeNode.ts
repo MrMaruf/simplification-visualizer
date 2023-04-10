43d57d4b64fbbd4ca522884f4d71d2de83394f02
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-04-10 17:24:26 +0200

    Finalized stringification of a tree node

--- src/models/TreeNode.ts
+++ src/models/TreeNode.ts
@@ -3,10 +3,27 @@ class TreeNode {
   left: TreeNode | null;
   right: TreeNode | null;
 
-  constructor(val: number) {
+  constructor(
+    val: number,
+    left: TreeNode | null = null,
+    right: TreeNode | null = null
+  ) {
     this.val = val;
-    this.left = null;
-    this.right = null;
+    this.left = left;
+    this.right = right;
+  }
+
+  toString(indent: string = ""): string {
+    let str = " " + this.val + "\n";
+    if (this.left && this.right) str += indent + "├";
+    else if (this.left || this.right) str += indent + "└";
+    if (this.left !== null) {
+      str += this.left.toString(indent + "│ ");
+    }
+    if (this.right !== null) {
+      str += indent + "└" + this.right.toString(indent + "  ");
+    }
+    return str;
   }
 }
 
