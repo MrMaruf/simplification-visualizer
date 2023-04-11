98f3f0473d0820b7e715259edba4c249661e78ff
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-04-11 13:43:30 +0200

    Fixed incorrect backwards conversion

--- src/models/MaxHeapBinaryTree.ts
+++ src/models/MaxHeapBinaryTree.ts
@@ -2,6 +2,23 @@ import BinaryTree from "./BinaryTree";
 import TreeNode from "./TreeNode";
 
 class MaxHeapBinaryTree extends BinaryTree {
+  traverse(
+    node: TreeNode | null,
+    array: Set<number>,
+    allowedDepth: number,
+    currentDepth: number = 0
+  ): void {
+    if (!node) {
+      return;
+    }
+
+    array.add(node.val);
+    if (node.left) array.add(node.left.val);
+    if (node.right) array.add(node.right.val);
+    if (allowedDepth <= currentDepth) return;
+    this.traverse(node.left, array, allowedDepth, currentDepth + 1);
+    this.traverse(node.right, array, allowedDepth, currentDepth + 1);
+  }
   arrayToTree(array: number[], currentIndex = 0): TreeNode | null {
     if (currentIndex >= array.length) {
       return null;
@@ -13,25 +30,18 @@ class MaxHeapBinaryTree extends BinaryTree {
 
     return root;
   }
-  treeToArray(): number[] {
-    const array: number[] = [];
-    if (!this.root) {
-      return array;
+  treeToArray(root: TreeNode | null = this.root): number[] {
+    const numberSet: Set<number> = new Set();
+    if (!root) {
+      return [];
+    }
+    const treeArray = this.traverseInOrder(this.root);
+    const maxDepth = (treeArray.length - 2) / 2 - 1;
+    for (let currentDepth = 0; currentDepth < maxDepth; currentDepth++) {
+      this.traverse(root, numberSet, currentDepth);
     }
 
-    const stack: TreeNode[] = [this.root];
-    while (stack.length) {
-      const node = stack.pop()!;
-      array.push(node.val);
-      if (node.right) {
-        stack.push(node.right);
-      }
-      if (node.left) {
-        stack.push(node.left);
-      }
-    }
-
-    return array;
+    return Array.from(numberSet).flat();
   }
 }
-export default MaxHeapBinaryTree
+export default MaxHeapBinaryTree;
